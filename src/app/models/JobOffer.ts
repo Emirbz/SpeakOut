7a3eef93707bc2e7e1a6c8f9b0ec43ff775ce889
khaoula sayadi <amir.benzineb@esprit.tn>
2021-06-09 23:12:44 +0100

    updated jobOffer model

--- src/app/models/JobOffer.ts
+++ src/app/models/JobOffer.ts
@@ -1,16 +1,21 @@
 import {Company} from './Company';
 
 export class JobOffer {
-  jobId?: number;
-  userId?: string;
-  jobTypeId?: number;
-  companyId?: number;
-  company?: Company
-  createdDate?: string;
-  jobDescription?: string;
-  isActive?: boolean;
-  isValid?: boolean;
-  jobLocationId?: number;
+  jobId: number;
+  userId: string;
+  jobTypeId: number;
+  companyId: number;
+  localisation: string;
+  title: string;
+  salaire: string;
+  categorie: string;
+  company: Company;
+  createdDate: string;
+  jobDescription: string;
+  isActive: boolean;
+  isValid: boolean;
+  jobLocationId: number;
+
 
   constructor() {
   }
