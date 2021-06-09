1d8f2430068b272e2678bd47ccd30452e8337738
khaoula sayadi <amir.benzineb@esprit.tn>
2021-06-10 00:51:43 +0100

    Finished create job offer interface

--- src/app/Site/Jobs/create-offer/create-offer.component.ts
+++ src/app/Site/Jobs/create-offer/create-offer.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {JobOfferService} from '../../../Services/job-offer.service';
+import {JobOffer} from '../../../models/JobOffer';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-create-offer',
@@ -9,8 +12,14 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 export class CreateOfferComponent implements OnInit {
   offerFormGroup: FormGroup;
   isFormSubmitted: boolean = false;
+  jobSalary: string[] = [];
+  jobCategory: string[] = [];
+  jobSalaryError: boolean = false;
+  jobCategoryError: boolean = false;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder,
+              private jobOfferService: JobOfferService,
+              private router: Router) {
   }
 
   ngOnInit(): void {
@@ -18,26 +27,72 @@ export class CreateOfferComponent implements OnInit {
   }
 
   offerFormValidate() {
+    // job salary static attributes
+    this.jobSalary = ['Job Salary', '28K € - 33K', '€ 33K € - 38K', '€ 38K € - 43 €', '43K € - 50K €', '+50K €'];
+
+    // job job category static attributes
+    this.jobCategory = ['Job Category', 'Graphic Designer', 'Engineering Jobs', 'Mainframe Jobs', 'Legal jobs', 'IT Jobs ', 'R&D Jobs', 'Government Jobs', 'PSU Jobs'];
+
+    // init formBuilder validators
     this.offerFormGroup = this.formBuilder.group({
       title: ['', Validators.required],
-
+      localisation: ['', Validators.required],
+      salaire: [this.jobSalary, Validators.required],
+      categorie: [this.jobCategory, Validators.required],
+      jobDescription: ['', Validators.compose([Validators.required, Validators.minLength(50)])],
     });
 
   }
 
 
   createOffer() {
-    this.isFormSubmitted = true;
-    if (this.offerFormGroup.valid) {
+    // check whenever default job salary value is still selected
+    if (this.offerFormGroup.value.salaire instanceof Array) {
+      this.jobSalaryError = true;
+    }
+    // check whenever default job category value is still selected
+    if (this.offerFormGroup.value.categorie instanceof Array) {
+      this.jobCategoryError = true;
+    }
 
-      console.log('success')
-    } else {
-      console.log('erreur')
+    this.isFormSubmitted = true;
+
+    // check if form is valid
+    if (this.offerFormGroup.valid
+      && !(this.offerFormGroup.value.salaire instanceof Array
+        && !(this.offerFormGroup.value.categorie instanceof Array))) {
+
+      // get form value and cast is as jobOffer
+      const jobOfferToSave = {...this.offerFormGroup.value} as JobOffer;
+
+      // set static user
+      jobOfferToSave.userId = '22788589-6ED2-CBAC-6800-3C04459C3D72';
+      // set random id
+      jobOfferToSave.jobId = Math.floor(Math.random() * 145879) + 1;
+      // set static company
+      jobOfferToSave.companyId = 167;
+      // save data to db
+      this.jobOfferService.createJobOffer(jobOfferToSave).subscribe(() => {
+        // on success navigate to job offers list
+        this.router.navigate(['/jobs']);
+      })
     }
 
   }
 
   formError(attribute: string, validator: string) {
     return this.offerFormGroup.get(attribute)?.hasError(validator);
+  }
+
+  resetSelect(attribute: 'SALARY' | 'CATEGORY') {
+    // reset error box status if select value has been changed
+    switch (attribute) {
+      case 'CATEGORY':
+        this.jobCategoryError = false;
+        break;
+      case 'SALARY':
+        this.jobSalaryError = false;
+        break;
+    }
   }
 }
